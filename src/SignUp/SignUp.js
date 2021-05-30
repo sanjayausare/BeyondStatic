267bfccfeaed5a692e989567ca2ba31a14460944
Sanjay Ausare <sanjaysudhir3@gmail.com>
2021-05-30 18:04:41 +0530

    Update SignUp.js

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -3,7 +3,7 @@ import SignUpForm from './SignUpForm'
 
 const SignUp = () => {
   return (
-    <div className="container-fluid row" style={{margin:'0'}}>
+    <div className="container-fluid row g-0" style={{margin:'0'}}>
         <SignUpForm/>
         <SignUpRightBanner/>
       </div>
