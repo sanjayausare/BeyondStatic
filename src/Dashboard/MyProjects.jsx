6f55a2fc7fc9f270ee030d79e7db955c66152ead
Devang Kamble <risingentropy20@gmail.com>
2021-06-02 00:49:29 +0530

    your projects

--- src/Dashboard/MyProjects.jsx
+++ src/Dashboard/MyProjects.jsx
@@ -1,10 +1,11 @@
 import styles from './MyProjects.module.css'
+import ProjectCard from './ProjectCard'
 
 const MyProjects = () => {
     return(
         <div style={{marginTop: '4%', marginLeft: '2%', marginRight: '0', padding: '0'}} className="container container-fluid">
             <h2 className={styles.myProj}><strong>My Projects (3)</strong></h2>
-            <div className="row">
+            <div className="row container-fluid" style={{marginTop: '3%'}}>
                 <ProjectCard/>
             </div>
         </div>
---
+++ src/Dashboard/ProjectCard.jsx
@@ -0,0 +1,11 @@
+import styles from './ProjectCard.module.css'
+
+const ProjectCard = () => {
+    return(
+        <>
+        hey
+        </>
+    )
+}
+
+export default ProjectCard;
