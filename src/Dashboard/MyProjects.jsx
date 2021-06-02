37c5f5c7436e5a69495fa591690282f76bbe6c90
Devang Kamble <risingentropy20@gmail.com>
2021-06-02 17:21:30 +0530

    god bless useeffect

--- src/Dashboard/MyProjects.jsx
+++ src/Dashboard/MyProjects.jsx
@@ -1,13 +1,43 @@
 import styles from './MyProjects.module.css'
 import ProjectCard from './ProjectCard'
+import axios from "axios";
+import {useState, useEffect} from 'react'
+import { getToken, getUsername, getURL } from "../utils/index";
 
 const MyProjects = () => {
 
+    const token = getToken();
+    const username = getUsername();
+    const url = getURL();
+    let projects = []
+    const [projList, setProjList] = useState('')
+
+    useEffect(
+      () => {
+        axios({
+          method: "GET",
+          url:
+            url + "/api/" + username + "/project",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${token}`,
+          },
+        })
+          .then((response) => {
+            setProjList(response.data)
+          })
+          .catch((err) => {
+            alert("Somthing went wrong!");
+          });
+      }, []
+    )
     
+
+        console.log(projList)
 
     return(
         <div style={{marginTop: '5%', marginLeft: '2%', marginRight: '2%', padding: '0', marginBottom: '3%'}} className="container container-fluid">
-            <h2 className={styles.myProj}><strong>My Projects (4)</strong></h2>
+            <h2 className={styles.myProj} style={{margin: '0', padding: '0', display: 'inline-block'}}><strong>My Projects ({projList.length})</strong></h2>
             <div className="row container-fluid" style={{marginTop: '3%'}}>
                 <div className="col-lg-4 col-md-4 col-sm-12"><ProjectCard status="Active"/></div>
                 <div className="col-lg-4 col-md-4 col-sm-12"><ProjectCard status="Inactive"/></div>
--- src/Dashboard/Overview/Overview.jsx
+++ src/Dashboard/Overview/Overview.jsx
@@ -6,7 +6,7 @@ import Info2 from './Info2'
 const Overview = () => {
     return(
         <div style={{marginTop: '5%', marginLeft: '2%', marginRight: '2%', padding: '0', marginBottom: '3%'}} className="container container-fluid">
-            <h2 className={styles.myProj}><strong>Overview:</strong></h2>
+            <h2 className={styles.myProj} style={{margin: '0', padding: '0', display: 'inline-block'}}><strong>Overview:</strong></h2>
             <div className="row container-fluid" style={{marginTop: '3%'}}>
                 <div className="col-lg-8 col-md-8 col-sm-12"><Chart/></div>
                 <div className="col-lg-2 col-md-2 col-sm-12"><Info1/></div>
