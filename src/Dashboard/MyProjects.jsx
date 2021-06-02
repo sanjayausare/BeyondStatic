d069f2bae50fb0718cb89e1631888cff8eb7626f
Devang Kamble <risingentropy20@gmail.com>
2021-06-02 19:49:31 +0530

    almost done with dashboard

--- src/Dashboard/MyProjects.jsx
+++ src/Dashboard/MyProjects.jsx
@@ -9,7 +9,6 @@ const MyProjects = () => {
     const token = getToken();
     const username = getUsername();
     const url = getURL();
-    let projects = []
     const [projList, setProjList] = useState('')
 
     useEffect(
@@ -31,18 +30,25 @@ const MyProjects = () => {
           });
       }, []
     )
-    
 
-        console.log(projList)
+    let listOfProjects = []
+    for(let i=0; i<projList.length; i++)
+    {
+      if(projList[i].ProjectStatus === true)
+      {listOfProjects.push(<div className="col-lg-4 col-md-4 col-sm-12"><ProjectCard projData={projList[i]} status="Active"/></div>)}
+      else
+      {listOfProjects.push(<div className="col-lg-4 col-md-4 col-sm-12"><ProjectCard projData={projList[i]} status="Inactive"/></div>)}
+    }
 
     return(
         <div style={{marginTop: '5%', marginLeft: '2%', marginRight: '2%', padding: '0', marginBottom: '3%'}} className="container container-fluid">
             <h2 className={styles.myProj} style={{margin: '0', padding: '0', display: 'inline-block'}}><strong>My Projects ({projList.length})</strong></h2>
             <div className="row container-fluid" style={{marginTop: '3%'}}>
-                <div className="col-lg-4 col-md-4 col-sm-12"><ProjectCard status="Active"/></div>
-                <div className="col-lg-4 col-md-4 col-sm-12"><ProjectCard status="Inactive"/></div>
-                <div className="col-lg-4 col-md-4 col-sm-12"><ProjectCard status="Active"/></div>
-                <div className="col-lg-4 col-md-4 col-sm-12"><ProjectCard status="Inactive"/></div>
+                {
+                  listOfProjects.map((item, index) => (
+                    item
+    ))
+                }
             </div>
         </div>
     )
--- src/Dashboard/ProjectCard.jsx
+++ src/Dashboard/ProjectCard.jsx
@@ -7,21 +7,23 @@ const ProjectCard = (props) => {
     activity = true;
   }
 
+  console.log(props.projData)
+
   const blueCard =  <div className={styles.blueCard} style={{ marginTop: "5%" }}>
   <div className="row">
     <div className="col-lg-8 col-sm-8">
-      <h2>Project Name</h2>
+      <h2>{props.projData.ProjectName}</h2>
     </div>
     <div className="col-lg-4 col-sm-4" style={{ textAlign: "right" }}>
       <h6>{props.status}</h6>
     </div>
   </div>
   <div className="row">
-    <div className="col-lg-12 col-sm-12" style={{ fontSize: "22px" }}>
-      <p>Description</p>
+    <div className="col-lg-12 col-sm-12" style={{ fontSize: "16px" }}>
+      <p>{props.projData.Description}</p>
     </div>
     <div className="col-lg-12 col-sm-12">
-      <p style={{ fontSize: "20px" }}>13 Entries</p>
+      <p style={{ fontSize: "20px" }}>{props.projData.totalMessages} entries</p>
     </div>
   </div>
 </div> 
@@ -29,18 +31,18 @@ const ProjectCard = (props) => {
 const pinkCard = <div className={styles.pinkCard} style={{ marginTop: "5%" }}>
 <div className="row">
   <div className="col-lg-8 col-sm-8">
-    <h2>Project Name</h2>
+    <h2>{props.projData.ProjectName}</h2>
   </div>
   <div className="col-lg-4 col-sm-4" style={{ textAlign: "right" }}>
     <h6>{props.status}</h6>
   </div>
 </div>
 <div className="row">
-  <div className="col-lg-12 col-sm-12" style={{ fontSize: "22px" }}>
-    <p>Description</p>
+  <div className="col-lg-12 col-sm-12" style={{ fontSize: "16px" }}>
+    <p>{props.projData.Description}</p>
   </div>
   <div className="col-lg-12 col-sm-12">
-    <p style={{ fontSize: "20px" }}>13 Entries</p>
+    <p style={{ fontSize: "20px" }}>{props.projData.totalMessages} entries</p>
   </div>
 </div>
 </div> 
